2acbf9a941a91b6cf6e36f7935548cd826a2d812
Raxuis <raclot.raphael@gmail.com>
2023-11-20 10:16:56 +0100

    First commit

---
+++ index.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Memento</title>
+    <link rel="stylesheet" href="style.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Glass+Antiqua&display=swap" rel="stylesheet">
+</head>
+
+<body>
+    <header>
+        <div class="container">
+            <nav>
+                <h2>Memento</h2>
+                <a href="login.php" title="Login">Login</a>
+                <a href="register.php" title="Register">Login</a>
+            </nav>
+        </div>
+    </header>
+    <main>
+        <div class="container">
+            <div class="title">
+                <h1>Memento</h1>
+                <button href="add.php">Add Postit</button>
+            </div>
+            <div class="postits">
+            </div>
+
+        </div>
+    </main>
+
+</body>
+
+</html>
